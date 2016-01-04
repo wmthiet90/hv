8fa624a98f3bf76d6b9259a487d68bfdf99e3e79
wmthiet <nvthiet.khtn08@gmail.com>
2016-01-04 23:48:49 +0700

    Init opencart for hv

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.hoaviet.local/admin/');
+define('HTTP_CATALOG', 'http://www.hoaviet.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.hoaviet.local/admin/');
+define('HTTPS_CATALOG', 'http://www.hoaviet.local/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\Thiet_web\xamp\htdocs\hoaviet/admin/');
+define('DIR_SYSTEM', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/');
+define('DIR_LANGUAGE', 'D:\Thiet_web\xamp\htdocs\hoaviet/admin/language/');
+define('DIR_TEMPLATE', 'D:\Thiet_web\xamp\htdocs\hoaviet/admin/view/template/');
+define('DIR_CONFIG', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/config/');
+define('DIR_IMAGE', 'D:\Thiet_web\xamp\htdocs\hoaviet/image/');
+define('DIR_CACHE', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/cache/');
+define('DIR_DOWNLOAD', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/download/');
+define('DIR_UPLOAD', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/upload/');
+define('DIR_LOGS', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/logs/');
+define('DIR_MODIFICATION', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/modification/');
+define('DIR_CATALOG', 'D:\Thiet_web\xamp\htdocs\hoaviet/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'hoaviet');
+define('DB_PREFIX', 'wm_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.hoaviet.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.hoaviet.local/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\Thiet_web\xamp\htdocs\hoaviet/catalog/');
+define('DIR_SYSTEM', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/');
+define('DIR_LANGUAGE', 'D:\Thiet_web\xamp\htdocs\hoaviet/catalog/language/');
+define('DIR_TEMPLATE', 'D:\Thiet_web\xamp\htdocs\hoaviet/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/config/');
+define('DIR_IMAGE', 'D:\Thiet_web\xamp\htdocs\hoaviet/image/');
+define('DIR_CACHE', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/cache/');
+define('DIR_DOWNLOAD', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/download/');
+define('DIR_UPLOAD', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/upload/');
+define('DIR_MODIFICATION', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/modification/');
+define('DIR_LOGS', 'D:\Thiet_web\xamp\htdocs\hoaviet/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'hoaviet');
+define('DB_PREFIX', 'wm_');
